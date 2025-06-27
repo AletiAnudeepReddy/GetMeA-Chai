7fa1ee6bca624bd3fd91534ccc83afbcdeccb9c5
anudeep096 <aletianudeepreddy8@gmail.com>
2025-06-27 13:13:38 +0530

    nothing

--- app/page.js
+++ app/page.js
@@ -10,12 +10,41 @@ export default function Home() {
       <p>
         A crowd funding platform for creators. Get funded by your fans and followers.
       </p>
-      <div>
+      <p>
+        A place where you fans can buy you a chai. Unleash the power of your fans and get your projects funded. 
+      </p>
+      <div className="flex gap-1">
         <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
         <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
       </div>
     </div>
     <div className="bg-white h-1 opacity-10"></div>
+    <div className="text-white container mx-auto mb-20">
+      <h1 className="text-3xl font-bold text-center my-16">Your Fans can buy you a Chai</h1>
+      <div className="flex gap-5 justify-around">
+        <div className="item flex space-y-3 flex-col items-center justify-center">
+          <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.gif" alt=""/>
+          <p className="font-bold">Fans want to help</p>
+          <p className="text-center">Your fans are available for you to help you.</p>
+        </div>
+        <div className="item flex space-y-3 flex-col items-center justify-center">
+          <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt=""/>
+          <p className="font-bold">Fans want to help</p>
+          <p className="text-center">Your fans are available for you to help you.</p>
+        </div>
+        <div className="item flex space-y-3 flex-col items-center justify-center">
+          <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt=""/>
+          <p className="font-bold">Fans want to help</p>
+          <p className="text-center">Your fans are available for you to help you.</p>
+        </div>
+      </div>
+    </div>
+    <div className="bg-white h-1 opacity-10"></div>
+    <div className="text-white container mx-auto pb-20 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-center my-16">Your Fans can buy you a Chai</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/nIiu4qqtJUk?si=9cxNGowx1d0QzRGL" 
+        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
     </>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='bg-gray-900 shadow-lg shadow-white text-white flex justify-between items-center px-4 h-16'>
-        <div className='logo font-bold text-lg'>GetMeaChai!</div>
+        <div className='logo font-bold text-lg flex justify-center items-center'>
+          <img src='/tea.gif' width={44} alt=''/>
+          <span>Get Me a Chai!</span>
+        </div>
         <ul className='flex justify-between gap-4'>
             <li>Home</li>
             <li>About</li>
