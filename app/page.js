5a894107ca0b2bed1d53df26beeb1704d7f1d26d
anudeep096 <aletianudeepreddy8@gmail.com>
2025-06-27 11:34:01 +0530

    header

--- app/page.js
+++ app/page.js
@@ -2,8 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      HomePage
+    <>
+    <div className="flex justify-center flex-col gap-4 items-center text-white h-[44vh]">
+      <div className="font-bold flex justify-center items-center gap-1 text-5xl">Buy Me a Chai<span className="mb-1">
+        <img src="/tea.gif" width={88}/>
+      </span></div>
+      <p>
+        A crowd funding platform for creators. Get funded by your fans and followers.
+      </p>
+      <div>
+        <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+        <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+      </div>
     </div>
+    <div className="bg-white h-1 opacity-10"></div>
+    </>
   );
 }
